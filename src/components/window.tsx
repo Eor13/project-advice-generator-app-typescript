401d2c5f909729a557b0fd60d6ec8146c2d0b58c
Everson de Oliveira <eor1309@gmail.com>
2023-07-23 15:05:00 -0300

    Correções Finais

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -23,11 +23,11 @@ const Show = () => {
             <S.Tittle>Advice #<span className="number-advice" >{showAdvice.slip.id}</span></S.Tittle>
             <S.Advice>{showAdvice.slip.advice}</S.Advice>
             <picture>
-                <source media="(max-width: 375px)" src="../../src/images/pattern-divider-mobile.svg" />
-                <S.Line src="../../src/images/pattern-divider-desktop.svg" />
+                <source media="(max-width: 375px)" src="../src/images/pattern-divider-mobile.svg" />
+                <S.Line src="../src/images/pattern-divider-desktop.svg" />
             </picture>
             <S.Btn onClick={changAdvice}>
-                <S.BtnImage src="../../src/images/icon-dice.svg" alt="Clique" />
+                <S.BtnImage src="../src/images/icon-dice.svg" alt="Clique" />
             </S.Btn>
         </S.AdviceCard>
     )
